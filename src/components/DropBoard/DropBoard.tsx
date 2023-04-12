474ef18e09766f8449eac58dcad019076b2f48f1
Serhii Serhatyi <serhiis@compatibl.com>
2023-04-12 14:35:31 +0300

    WIP state on horizontal dnd

--- src/components/DropBoard/DropBoard.tsx
+++ src/components/DropBoard/DropBoard.tsx
@@ -1,13 +1,25 @@
-import React, { useCallback, useRef, useState } from "react";
-import "./DropBoard.scss";
+import React, { useCallback, useState } from "react";
 import { landerRenderer } from "../../utils/helpers/lenderRenderer";
+import "./DropBoard.scss";
 
 export const DropBoard = () => {
-  const dropArea = useRef<HTMLDivElement>(null);
-
   const [draggingEl, setDragginEl] = useState<EventTarget | null>(null);
+  const [sections, setSections] = useState([
+    {
+      id: 1,
+      order: 1
+    },
+    {
+      id: 2,
+      order: 2
+    },
+    {
+      id: 3,
+      order: 3
+    }
+  ]);
 
-  const handleDrop = (event: React.DragEvent) => {
+  const handleDrop = (event: React.DragEvent, item: any) => {
     event.preventDefault();
 
     const element_id = event.dataTransfer.getData("widgetType");
@@ -15,34 +27,39 @@ export const DropBoard = () => {
 
     const lander: HTMLElement | null = document.querySelector(".lander");
 
-    if (lander?.id === 'right-col') {
-      dropArea.current?.append(element);
+    if (lander?.id === "right-col") {
+      setSections([item, ...sections.filter(section => section != item)]);
     }
 
-    if (lander?.id === 'left-col') {
-      dropArea.current?.prepend(element);
+    if (lander?.id === "left-col") {
+      console.log(sections.filter(section => section != item));
+      setSections([...sections.filter(section => section != item), item]);
     }
 
-    element.removeAttribute('style');
+    element.removeAttribute("style");
 
-    lander?.remove();
+    document.querySelector('.manager')?.classList.remove('dragging');
+
+    setDragginEl(null);
   };
 
-  const handleDragOver = useCallback((event: React.DragEvent) => {
-    event.preventDefault();
+  const handleDragOver = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
 
-    const lander: HTMLElement | null = document.querySelector(".lander");
+      const lander: HTMLElement | null = document.querySelector(".lander");
 
-    (draggingEl as HTMLElement).style.top = event.clientY + 'px';
-    (draggingEl as HTMLElement).style.left = event.clientX + 'px';
-    (draggingEl as HTMLElement).style.transform = 'scale(0.1)';
-    (draggingEl as HTMLElement).style.transformOrigin = '0 0';
+      (draggingEl as HTMLElement).style.top = event.clientY + "px";
+      (draggingEl as HTMLElement).style.left = event.clientX + "px";
+      (draggingEl as HTMLElement).style.transform = "scale(0.1)";
+      (draggingEl as HTMLElement).style.transformOrigin = "0 0";
 
-    const rect: DOMRect | undefined =
-      dropArea.current?.getBoundingClientRect()!;
+      const rect = document.querySelector('.manager')?.getBoundingClientRect();
 
-    landerRenderer(lander!, event, rect!);
-  }, [draggingEl]);
+      landerRenderer(lander!, event, rect!);
+    },
+    [draggingEl, sections]
+  );
 
   const handleDragStart = (event: React.DragEvent, widgetType: string) => {
     event.dataTransfer.setData("widgetType", widgetType);
@@ -50,29 +67,38 @@ export const DropBoard = () => {
 
     setDragginEl(event.target);
 
-    (event.target as HTMLElement).style.position = 'absolute';
+    (event.target as HTMLElement).style.position = "absolute";
 
-    const newDiv = document.createElement('div');
-
-    newDiv.classList.add('lander');
-
-    dropArea.current?.appendChild(newDiv);
+    document.querySelector('.manager')?.classList.add('dragging');
   };
 
   const dissablePointerEvents = (event: React.DragEvent) => {
-    (event.target as HTMLElement).style.pointerEvents = 'none';
-  }
+    (event.target as HTMLElement).style.pointerEvents = "none";
+  };
 
   return (
-    <div className="drop-area_root">
-      <div
-        ref={dropArea}
-        className="drop-area_row"
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-      >
-        <div className="drag-item q1" draggable onDragStart={(e) => handleDragStart(e, "drag-item q1")} onDrag={dissablePointerEvents}></div>
-        <div className="drag-item q2" draggable onDragStart={(e) => handleDragStart(e, "drag-item q2")} onDrag={dissablePointerEvents}></div>
+    <div className="container">
+      <div className="manager">
+        <div
+          className="drop-area_row"
+        >
+          {
+            sections.map((section) => {
+              return (
+                <div
+                  key={section.id}
+                  className={`drag-item q${section.id}`}
+                  draggable
+                  onDragStart={(e) => handleDragStart(e, `drag-item q${section.id}`)}
+                  onDragOver={handleDragOver}
+                  onDrag={dissablePointerEvents}
+                  onDrop={(e) => handleDrop(e, section)}
+                ></div>
+              );
+            })
+          }
+        </div>
+        <div className="lander" />
       </div>
     </div>
   );
